bf6f07096be336e23d0871303aa42b9577dbddcc
Vanushka <30829997+Vanushka@users.noreply.github.com>
2018-12-10 22:04:31 +0300

    puted in a tab in the false function so that it does not give # to the url

--- dist/js/script.js
+++ dist/js/script.js
@@ -241,6 +241,7 @@ $(document).ready(function(){
         $(".text").hide().removeClass("active"); // Скрываем и удаляем активный класс у прошлого контейнера с содержимым
         $(this).addClass("active"); // Добавляем нажатому пункту меню активный класс
         $($(".block-text").children(".text")[i]).fadeIn(1000).addClass("active"); // Показываем и добавляем активный класс соответствующему контейнеру
+        return false;
       }
     });
   });
